1ffa0bba3c770c5721dfdf2d1a0f8d6060634ff6
Connor Walsh <cwalsh24@ut.utm.edu>
2022-08-25 11:51:14 -0500

    Fourth commit: adding updated main.cpp file

--- main.cpp
+++ main.cpp
@@ -3,8 +3,23 @@
 
 using namespace std;
 
+int sum(int n); //prototype for sum method
+
 int main() {
-  cout<<"Hello World!"<<endl; 
+  cout<<"Hello World!"<<endl;
+  int n;
+  cout<<"Enter a number: ";
+  cin>>n;
+  cout<<sum(n)<<endl; //prints the answer 
+  
   
   return 0;
+}
+
+int sum(int n){
+  int sum = 0; 
+  for(int i; i<=n; i++){
+    sum+= n-i;
+  }
+  return sum;
 }
